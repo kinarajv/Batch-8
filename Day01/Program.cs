88249973bc65e58714902833f16629aa7052864a
Kinara <91768638+kinarajv@users.noreply.github.com>
2023-12-18 16:31:16 +0700

    first commit

---
+++ Day01/Program.cs
@@ -0,0 +1,8 @@
+﻿class Program
+{
+    static void Main() //Methode
+    {
+        // See https://aka.ms/new-console-template for more information
+        Console.WriteLine("Hello, World!");
+    }
+}
