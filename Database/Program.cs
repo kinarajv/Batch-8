2a6fcebcad03453288aa9a442e41d6873865b97d
Kinara <91768638+kinarajv@users.noreply.github.com>
2023-12-22 11:09:11 +0700

    Add CUD

--- Database/Program.cs
+++ Database/Program.cs
@@ -2,11 +2,11 @@
 using Database;
 using Microsoft.EntityFrameworkCore;
 
-class Program 
+class Program
 {
-	static void Main() 
+	static async Task Main()
 	{
-		using(Northwind db = new Northwind()) 
+		using (Northwind db = new Northwind())
 		{
 			// List<Category> categories = db.Categories.Include(c => c.Products).ToList();
 			// foreach(var c in categories) 
@@ -18,11 +18,39 @@ class Program
 			// 		Console.WriteLine("\t" + p.ProductName);
 			// 	}
 			// }
-			var products = db.Products.Where(p => p.UnitPrice > 1000);
-			foreach(var p in products) 
-				{
-					Console.WriteLine("\t" + p.ProductName + " price " + p.UnitPrice);
-				}
+
+			// .Where() = Multiple
+			// .FirtsOrDefault() = Single
+
+			//Read
+			// Product? product = await db.Products.FirstOrDefaultAsync(p => p.UnitPrice > 1000);
+			// IQueryable<Product> products = db.Products.Where(p => p.UnitPrice > 1000);
+			// foreach (var p in products)
+			// {
+			// 	Console.WriteLine("\t" + p.ProductName + " price " + p.UnitPrice);
+			// }
+
+			//CUD => SaveChanges
+			//Create
+			//Category
+			Category category = new Category()
+			{
+				Description = "Ini Electronic"
+			};
+			await db.Categories.AddAsync(category);
+			await db.SaveChangesAsync();
+
+			//Update
+			// Category category = await db.Categories.FindAsync(9);
+			// Category category2 = await db.Categories
+			// 							.FirstOrDefaultAsync(c => c.CategoryId == 9);
+			// category.CategoryName = "Elektronik";
+			// await db.SaveChangesAsync();	
+			
+			//Delete
+			// Category category = await db.Categories.FindAsync(9);
+			// db.Categories.Remove(category);
+			// await db.SaveChangesAsync();	
 		}
 	}
 }
